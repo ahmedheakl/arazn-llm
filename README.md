c98824929f0c00a76e3af01462c088f30e86ef6e
Heakl <ahmed.heakl@ejust.edu.eg>
2024-06-22 23:10:28 +0300

    Added text input

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+---
+title: arazn-llm
+app_file: app.py
+sdk: gradio
+sdk_version: 4.31.4
+---
 # AraZn-LLM
 Code-Switched translations with Large Language models
 
--- app.py
+++ app.py
@@ -5,41 +5,43 @@ import numpy as np
 
 ENG_MODEL = "arazn-llama3-eng"
 WHISPER_SMALL = "ahmedheakl/arazn-whisper-small-v2"
+TEMPERATURE=0.6
+TOP_P=0.9
 
 transcriber = pipeline("automatic-speech-recognition", model=WHISPER_SMALL, device="cuda:0")
 
 def generate_text_eng(prompt):
     response = ollama.chat(model=ENG_MODEL, messages=[
         {"role": "user", "content": prompt}
-    ])
+    ], options=ollama.Options(num_gpu=1, main_gpu=1, temperature=TEMPERATURE, top_p=TOP_P))
     return response['message']['content']
 
-
-def transcribe(stream, audio):
+def transcribe(stream, audio, text_input=None):
+    if text_input is not None:
+        return None, text_input, generate_text_eng(text_input)
+    
     sr, y = audio
     y = y.astype(np.float32)
+    print(y)
     y /= np.max(np.abs(y))
 
-    if stream is not None:
+    if stream is not None and stream.shape[0] < 500_000:
         stream = np.concatenate([stream, y])
     else:
         stream = y
 
     prompt = transcriber({"sampling_rate": sr, "raw": stream})["text"]
-    return stream, prompt, generate_text_eng(prompt)
+    to_be_translated = prompt.split(".")[-1]
+    return stream, prompt, generate_text_eng(to_be_translated)
 
-
-
-
-
-d = gradio.Interface(   
+demo = gradio.Interface(   
     transcribe, 
-    ["state", gradio.Audio(label="Upload Audio", sources=["microphone"], streaming=True)],
-    ["state", "text", "text"],
-    # ["state", gradio.Textbox(label="Transcription"), gradio.Textbox(label="Translation")],
+    ["state", gradio.Audio(label="Upload Audio", sources=["microphone"], streaming=True), gradio.Textbox(label="Or Enter Text")],
+    ["state", gradio.Textbox(label="Transcription"), gradio.Textbox(label="Translation (English)")],
     title="Whisper to Ollama",
-    description="Upload an audio clip and get a response from the Ollama AI.",
+    description="Upload an audio clip or enter text and get a response from the Ollama AI.",
     live=True
 )
 
-d.launch(share=True)
+if __name__ == "__main__":
+    demo.launch(share=True)
